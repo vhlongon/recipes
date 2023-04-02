6126c241db93b55058ece046730ecf165af4e4a1
victorlongon <victor.longon@hiberworld.com>
2023-04-03 00:22:24 +0200

    create a button component using daisyui and cva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from '@/components/Button';
 import { Inter } from 'next/font/google';
 
 export default function Home() {
-  return <main>main</main>;
+  return (
+    <main>
+      <Button variant="primary">click me</Button>
+    </main>
+  );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,76 @@
+import clsx from 'clsx';
+import React from 'react';
+import { VariantProps, cva } from 'class-variance-authority';
+
+type BaseProps = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+const buttonClasses = cva(['btn'], {
+  variants: {
+    variant: {
+      primary: ['btn-primary'],
+      secondary: ['btn-secondary'],
+      accent: ['btn-accent'],
+      ghost: ['btn-ghost'],
+      link: ['btn-link'],
+    },
+    size: {
+      sm: ['btn-sm'],
+      md: ['btn-md'],
+      lg: ['btn-lg'],
+    },
+    outline: {
+      true: ['btn-outline'],
+    },
+    square: {
+      true: ['btn-square'],
+    },
+    circle: {
+      true: ['btn-circle'],
+    },
+    loading: {
+      true: ['loading'],
+    },
+    block: {
+      true: ['btn-block'],
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+    variant: 'primary',
+  },
+});
+type ButtonProps = BaseProps & VariantProps<typeof buttonClasses>;
+
+export const Button = ({
+  variant,
+  size,
+  outline,
+  square,
+  circle,
+  loading,
+  block,
+  children,
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={buttonClasses({
+        size,
+        variant,
+        className,
+        outline,
+        square,
+        circle,
+        loading,
+        block,
+      })}
+    >
+      {children}
+    </button>
+  );
+};
