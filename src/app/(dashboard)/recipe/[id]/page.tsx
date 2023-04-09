045eb4bd3d53596ff482d31d2ff16a44c7457ff9
victorlongon <victor.longon@hiberworld.com>
2023-04-09 10:51:16 +0200

    scaffold recipe page

--- src/app/(dashboard)/recipe/[id]/page.tsx
+++ src/app/(dashboard)/recipe/[id]/page.tsx
@@ -1,10 +1,28 @@
+import { getUserRecipe } from '@/lib/data';
+
 type RecipePageProps = {
   params: {
     id: string;
   };
 };
-const RecipePage = ({ params }: RecipePageProps) => {
-  return <div>RecipePage: {params.id}</div>;
+const RecipePage = async ({ params }: RecipePageProps) => {
+  const data = await getUserRecipe(params.id);
+
+  return (
+    <div>
+      RecipePage for recipe: {params.id}
+      <div className="mt-8">
+        {data &&
+          Object.keys(data).map((key, index) => {
+            return (
+              <div key={index}>
+                {key}: {JSON.stringify(data[key])}
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default RecipePage;
