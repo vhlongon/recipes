17a3eb5e984ba8463c40a056192e3b8bf71ab1f3
victorlongon <victor.longon@hiberworld.com>
2023-04-09 10:50:36 +0200

    update home loading

--- src/app/(dashboard)/home/loading.tsx
+++ src/app/(dashboard)/home/loading.tsx
@@ -1,26 +1,8 @@
 import { RecipeCardSkeleton } from '@/components/RecipeCard';
-import { getUserFromCookies } from '@/lib/cookies';
-import { db } from '@/lib/db';
-import { cookies } from 'next/headers';
-
-const getData = async () => {
-  const user = await getUserFromCookies(cookies());
-
-  if (!user) {
-    return 0;
-  }
-
-  const totalRecipes = await db.recipe.count({
-    where: {
-      userId: user.id,
-    },
-  });
-
-  return totalRecipes;
-};
+import { getUserRecipeCount } from '@/lib/data';
 
 const DashBoardLoader = async () => {
-  const totalRecipes = await getData();
+  const totalRecipes = await getUserRecipeCount();
   return (
     <div className="grid gap-6 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 p-6 max-h-panel scrollable  overflow-y-scroll">
       {Array.from({ length: totalRecipes }, (_, i) => {
