79f5d7e4e203472785673bc51567905a75847e26
victorlongon <victor.longon@hiberworld.com>
2023-04-09 10:52:10 +0200

    get user info into profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,22 @@
+import { getUser } from '@/lib/data';
+import { formatDate } from '@/lib/date';
 import React from 'react';
 
-const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+const ProfilePage = async () => {
+  const user = await getUser();
+
+  if (!user) {
+    return <div>Not logged in</div>;
+  }
+
+  return (
+    <div>
+      <div>Firstname: {user.firstName}</div>
+      <div>LastName: {user.lastName}</div>
+      <div>Registered: {formatDate(user.createdAt)}</div>
+      <div>Email: {user.email}</div>
+    </div>
+  );
 };
 
 export default ProfilePage;
