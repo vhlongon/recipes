c64acbba970de35f3d8a3404de717aaa1f3b8908
victorlongon <victor.longon@hiberworld.com>
2023-04-12 09:56:12 +0200

    add api route to handle user image upload

---
+++ src/app/api/user-image/route.ts
@@ -0,0 +1,56 @@
+import { getUserFromCookies } from '@/lib/cookies';
+import { updateUserImage } from '@/lib/data';
+import { uploadImage } from '@/lib/image';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  try {
+    const user = await getUserFromCookies(cookies());
+
+    if (!user) {
+      const message = 'Not authorized';
+      return NextResponse.json(
+        { data: { message } },
+        {
+          status: 403,
+          statusText: message,
+        }
+      );
+    }
+
+    const url = await uploadImage(body.image);
+    const image = await updateUserImage(url);
+
+    if (!image) {
+      const message = 'could not update user image';
+
+      return NextResponse.json(
+        { data: { message } },
+        {
+          status: 500,
+          statusText: message,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      { data: { image } },
+      {
+        status: 200,
+        statusText: 'ok',
+      }
+    );
+  } catch (error: any) {
+    const message = `could not upload user image: ${error.message}`;
+    return NextResponse.json(
+      { data: { message } },
+      {
+        status: 500,
+        statusText: message,
+      }
+    );
+  }
+}
