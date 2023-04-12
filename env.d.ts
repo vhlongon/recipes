81fa8a314b70491c87d6c691406c57c1126118e7
victorlongon <victor.longon@hiberworld.com>
2023-04-12 09:53:33 +0200

    install cloudinary and add env vars

--- env.d.ts
+++ env.d.ts
@@ -8,6 +8,9 @@ declare global {
       OPENAI_API_KEY: string;
       PROTECT_COOKIE_NAME: string;
       PROTECT_SECRET: string;
+      CLOUDINARY_CLOUD_NAME: string;
+      CLOUDINARY_API_KEY: string;
+      CLOUDINARY_API_SECRET: string;
     }
   }
 }
